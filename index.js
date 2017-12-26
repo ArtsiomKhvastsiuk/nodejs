9ada0335133817fab32c45982a6d7af8d32acc6a
Artsiom Khvastsiuk <Artsiom_Khvastsiuk@epam.com>
2017-12-26 17:58:38 +0300

    creating a file to csv to json

--- index.js
+++ index.js
@@ -1,71 +1,25 @@
-/*
- const DirWathcer = require('./src/dirwatcher');
- const Importer = require('./src/importer');
+const DirWathcer = require('./src/dirwatcher');
+const Importer = require('./src/importer');
 
- const PATH  = './data';
+const PATH = './data';
 
- const watcher = new DirWathcer();
- const importer = new Importer();
+const watcher = new DirWathcer();
+const importer = new Importer();
 
- watcher.watch(PATH);
- watcher.on('change', (path) => {
- importer.import(path)
- .then((files) => {
- console.log(files);
- })
- .catch((error) => {
- console.log(error);
- });
+watcher.watch(PATH);
+watcher.on('change', (path) => {
+    importer.import(path)
+        .then((files) => {
+            console.log(files);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
 
- try {
- const files = importer.importSync(path);
- console.log(files);
- } catch(e) {
- console.log(e);
- }
- });
- */
-
-const fs = require('fs');
-const through = require('through2');
-
-
-function write(buffer, encoding, next) {
-    const separator = ',';
-    const json = [];
-    let columnNames = null;
-    let isColumn = true;
-    const lines = buffer.toString().split('\n');
-
-    lines.map((line, index) => {
-        if (isColumn) {
-            columnNames = line.split(separator);
-            isColumn = false;
-        } else {
-            const columns = line.match((/("[^"]+"|[^,]+)/g));
-            const row = {};
-            if (columns) {
-                columns.map((line, index) => {
-                    row[columnNames[index]] = line;
-                });
-            } else return;
-            json.push(row);
-        }
-    });
-    const jsonString = JSON.stringify(json);
-    this.push(jsonString);
-    next();
-}
-
-const stream = through(write, /*end*/);
-
-function csvToJson(csvFile, JSONdest) {
-    const all = [];
-    const reader = fs.createReadStream(csvFile);
-    reader.pipe(stream).pipe(process.stdout);
-    reader.on('data', (data) => {
-        all.push(data);
-    });
-}
-
-csvToJson('./data/MOCK_DATA.csv');
+    try {
+        const files = importer.importSync(path);
+        console.log(files);
+    } catch (e) {
+        console.log(e);
+    }
+});
--- utils/streams.js
+++ utils/streams.js
@@ -1,15 +1,20 @@
 #!/usr/bin/env node
 
-const program = require('commander');
-const through = require('through2');
 const fs = require('fs');
 const path = require('path');
+const program = require('commander');
+const through = require('through2');
 
 function choiceAction(config) {
-    const {action, fileName} = config;
+    const {action, fileName, withCreating} = config;
     switch (action) {
         case 'io':
             inputOutput(fileName);
+            break;
+        case 'transform':
+            csvToJson(fileName, withCreating);
+            break;
+        default: return undefined;
     }
 }
 
@@ -23,7 +28,7 @@ function inputOutput(filePath) {
     } else {
         process.stdin.setEncoding('utf8');
         // why end function doesn't emit?
-        const stream = through(write, /*end*/);
+        const stream = through(IOTransform /*,end*/);
         process.stdin.pipe(stream).pipe(process.stdout);
         stream.on('end', () => {
             process.exit(0);
@@ -31,10 +36,66 @@ function inputOutput(filePath) {
     }
 }
 
+function IOTransform(chunk, encoding, next) {
+    const data = chunk.toString();
+    this.push(data.toUpperCase());
+    this.push(null);
+    next();
+}
+
+function csvToJson(csvFile, withCreating) {
+    const all = [];
+    const stream = through(csvToJSONTransform /*,end*/);
+    const reader = fs.createReadStream(csvFile);
+
+    if (withCreating) {
+        const filename = path.basename(csvFile).slice(0, -3);
+        const writer = fs.createWriteStream(filename + '.json');
+        reader.pipe(stream).pipe(writer);
+        reader.on('data', (data) => {
+            all.push(data);
+        });
+    } else {
+        reader.pipe(stream).pipe(process.stdout);
+        reader.on('data', (data) => {
+            all.push(data);
+        });
+    }
+}
+
+function csvToJSONTransform(buffer, encoding, next) {
+    const separator = ',';
+    const json = [];
+    let columnNames = null;
+    let isColumn = true;
+    const lines = buffer.toString().split('\n');
+
+    lines.map((line, index) => {
+        if (isColumn) {
+            columnNames = line.split(separator);
+            isColumn = false;
+        } else {
+            const columns = line.match((/("[^"]+"|[^,]+)/g));
+            const row = {};
+            if (columns) {
+                columns.map((line, index) => {
+                    row[columnNames[index]] = line;
+                });
+            } else return;
+            json.push(row);
+        }
+    });
+
+    const jsonString = JSON.stringify(json, null, 2);
+    this.push(jsonString);
+    next();
+}
+
 program
     .version('0.0.0')
     .option('-a, --action <string>', 'action name')
-    .option('-f, --file <string>', 'path to directory');
+    .option('-f, --file <string>', 'path to directory')
+    .option ('-d --create', 'the flag to create a file');
 
 program.parse(process.argv);
 
@@ -42,17 +103,11 @@ const WITH_NO_COMMANDS = process.argv.slice(2).length === 0;
 const config = {
     action: program.action,
     fileName: program.file,
+    withCreating: program.create
 };
 // if no commands were inputted print a usage message
 if (WITH_NO_COMMANDS) {
     program.help();
 } else {
     choiceAction(config);
-}
-
-function write(chunk, encoding, next) {
-    const data = chunk.toString();
-    this.push(data.toUpperCase());
-    this.push(null);
-    next();
 }
